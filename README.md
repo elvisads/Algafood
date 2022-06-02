1296acc6b7685b1b25645951d8358fd3b6206864
elvisads <elvisprojetoss@gmail.com>
2022-06-02 19:02:07 -0300

    Organizando README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Algafood
-Primeiro commit, início ao projeto
+<h4 align="center">
+Projeto README Em construção...
+</h4>
+
+## Conteúdo e ferramentas utilizados no projeto
+
+- [x] Introdução
+- [x] Spring e Injeção de Dependências
+- [x] Introdução ao JPA e Hibernate
+- [x] REST com Spring
+- [ ] Spring Data JPA
+- [ ] Explorando mais do JPA e Hibernate
+- [ ] Pool de conexões e Flyway
+- [ ] Tratamento e modelagem de erros da API
+- [ ] Validações com Bean Validation
+- [ ] Testes de integração
+- [ ] Boas práticas e técnicas para APIs
+- [ ] Modelagem avançada e implementação da API
+- [ ] Modelagem de projeções, pesquisas e relatórios
+- [ ] Upload e download de arquivos
+- [ ] E-mails transacionais e Domain Events
+- [ ] CORS e consumo da API com JavaScript e Java
+- [ ] Cache de HTTP
+- [ ] Documentação da API com OpenAPI, Swagger UI e SpringFox
+- [ ] Discoverability e HATEOAS: A Glória do REST
+- [ ] Evoluindo e versionando a API
+- [ ] Logging
+- [ ] Segurança com Spring Security e OAuth2
+- [ ] OAuth2 avançado com JWT e controle de acesso
+- [ ] Dockerizando a aplicação
+- [ ] Deploy em containers Docker na Amazon
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+
+@Service
+public class CadastroCidadeService {
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	public Cidade salvar(Cidade cidade) {
+		
+		return cidadeRepository.salvar(cidade);
+		
+	}
+	
+	public void excluir(Long cidadeId) {
+		try {
+			cidadeRepository.remover(cidadeId);	
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(String.format("Não existe um cadastro de cidade com código %d", cidadeId));
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Cozinha de codigo %d não pode ser removida pois está em uso", cidadeId));
+		}
+		
+		
+	}
+}
