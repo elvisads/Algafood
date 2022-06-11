23ddb2f2c18616406b76099b895a9c1d892b2009
elvisads <elvisprojetoss@gmail.com>
2022-06-11 04:41:09 -0300

    Criando teste busca por Query String

--- src/main/java/com/algaworks/algafood/infrainstructore/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrainstructore/repository/CozinhaRepositoryImpl.java
@@ -6,13 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
@@ -22,6 +22,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 	public List<Cozinha> listar() {
 		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 		
+	}
+	
+	@Override
+	public List<Cozinha> consultarPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();
 	}
 	
 	@Override
